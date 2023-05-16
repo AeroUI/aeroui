2c405bcc717943899258a49661d6405d9c7b07d4
shabman <avia.shabbyman@gmail.com>
2023-05-16 23:41:53 +0100

    glfw added

---
+++ scripts/link.sh
@@ -0,0 +1,3 @@
+# !bin/bash
+
+g++ -std=c++11 ./src/main.cpp `pkg-config --cflags --static --libs glfw3` `pkg-config --libs --cflags glew glm` -framework OpenGL
---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#define GL_SILENCE_DEPRECATION
+#define GLEW_STATIC
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int main(void) {
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return EXIT_FAILURE;
+
+    window = glfwCreateWindow(1080, 720, "AeroUI", NULL, NULL);
+
+    if (!window) {
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return EXIT_SUCCESS;
+}
