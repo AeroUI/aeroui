f13a42e131b66d579f47d3dae40ef3128c2e938d
Kaushik-Kalesh <kaushikkalesh@gmail.com>
2023-10-27 17:14:17 +0530

    added Linux support

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,3 +1,9 @@
-# !bin/bash
+#!/bin/bash
 
-g++ -std=c++11 ./src/main.cpp -I./include `pkg-config --cflags --static --libs glfw3` `pkg-config --libs --cflags glew glm` -framework OpenGL
+unamestr=$(uname)
+
+if [ "$unamestr" == "Linux" ]; then
+    g++ -std=c++11 ./src/main.cpp -I./include $(pkg-config --cflags --static --libs glfw3) -L/path/to/opengl/lib -lGL -o main.o
+elif [ "$unamestr" == "Darwin" ]; then
+    g++ -std=c++11 ./src/main.cpp -I./include $(pkg-config --cflags --static --libs glfw3) $(pkg-config --libs --cflags glew glm) -framework OpenGL -o main.o
+fi
